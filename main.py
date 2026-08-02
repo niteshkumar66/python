06933dbab1fba1416416752ac15efefc1bbf1512
niteshkumar66 <nitesh0221kumar@gmail.com>
2026-08-02 23:40:23 +0530

    conversion

--- main.py
+++ main.py
@@ -6,15 +6,64 @@ teachingFrom = 'Sheriyans'
 TeacherIs = 'Anuj'
 teacher_experience = True
 
-print('Namaste Youtube we are learning python')
+# print('Namaste Youtube we are learning python')
 
-python = 'Learning'
 
 a = 12.892
 
 v = 34j
-print(type(v))
+# print(type(v))
 
-print(type (name))
+# print(type (name))
 
-print(type (teacher_experience))
+# print(type (teacher_experience))
+
+a = 'A'
+# print(ord(a))
+
+b = 65
+# print(chr(b))
+
+python = 'Learning with Sheriyans'
+# print(python[-2])
+
+identity = 'SHER'
+# print(identity[1])
+# print(identity[-1])
+
+line = 'He is a very good coder and also known as SHER CODER'
+
+# print(line[0::1])            # Defaulty go to the end of the line 
+# print(line[:23:1])           # Defaulty start with the zero index of the line 
+# print(line[0:23:])           # Defaulty hower to one by one index
+
+
+dog =  14
+print(type(dog))
+print(type(str(dog)))
+
+
+# a = '12abc3'            # cannot convert alphabet string to integer
+# a = int(a)
+
+# print(type(a))
+
+
+string = 'hello'
+print(bool(string))
+
+
+string = 0
+print(bool(string))
+
+
+# falsy values 
+
+'''false 
+0
+0.0
+""
+[]
+{}
+()
+'''
