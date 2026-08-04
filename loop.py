c550acc058b3d1198fbc2dedfbc1f2ba43315643
niteshkumar66 <nitesh0221kumar@gmail.com>
2026-08-04 12:45:20 +0530

    loops

---
+++ loop.py
@@ -0,0 +1,22 @@
+# For loop
+
+# for i in range (1,20,1):
+#     print(i)
+
+# for i in range(20,51):
+#     print(i)
+
+# for i in range(16,0,-1):
+#     print(i)
+
+# for i in range (-5,-15,-1):
+#     print(i)
+
+
+# for i in range(5,51,5):
+    # print(i)
+
+n = int(input("which table u want ? "))
+
+for i in range(n,(n*10)+1 , n):
+    print(i)
