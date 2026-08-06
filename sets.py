4ec04c1458494707af7586ec8df7358b105e99b2
niteshkumar66 <nitesh0221kumar@gmail.com>
2026-08-06 15:23:42 +0530

    sets

---
+++ sets.py
@@ -0,0 +1,13 @@
+# SETS
+
+# Mutable : change of any value is allowed 
+# duplicate : there can be multiple same Value 
+# Unordered : elements cannot be accessed by index value 
+# semi-heterogenous : it can store some different datatypes (string , number, tuples ) but not all 
+
+s = {1,2,3,4,5,6,5}
+
+
+
+print(s)
+
