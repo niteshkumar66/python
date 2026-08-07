4045e4b6f4c0594cdaeccd10f6a65e918ce065c8
niteshkumar66 <nitesh0221kumar@gmail.com>
2026-08-08 01:43:32 +0530

    dict

--- dictionary.py
+++ dictionary.py
@@ -6,14 +6,39 @@
 # heterogenous : allow to store multiple datatype in a single list  
 
 
-d = {}
-print(type(d))
+# d = {}
+# print(type(d))
 
-a = {
-    1: "Hello",
-    "name": "Nitesh Kumar",
-    "age" : 23
-}
+# a = {
+#     1: "Hello",
+#     "name": "Nitesh Kumar",
+#     "age" : 23
+# }
 
-print(a)
-print(a["name"])
+# print(a)
+# print(a["name"])
+
+
+d = {1: 100, 2: 200 , 3: 300, 4: 400}
+
+# d[1] = 1000
+# d.update({50: 500})       # Updating 
+# d[50] = 500                 # Creating 
+# del d[2]                    # Deleting 
+
+# print(d)
+
+
+# for i in d:
+#     print(d[i])
+
+# for i in d.keys():
+#     print(i)
+
+# for i in d.values():
+#     print(i)
+
+# d.clear()                 # clear the all key and value
+
+
+help(dict)
