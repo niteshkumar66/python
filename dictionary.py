42a5fd7278c42339cd6d184c68986f0240dbec8f
niteshkumar66 <nitesh0221kumar@gmail.com>
2026-08-07 23:57:32 +0530

    dictonary

---
+++ dictionary.py
@@ -0,0 +1,19 @@
+# Dictonary
+
+# Mutable : Semi Mutable , keys can't be change but the value can be changed 
+# Duplicate : Key must be unique but the value can be duplicate (same)
+# Ordered : Dictonary follows insertion order
+# heterogenous : allow to store multiple datatype in a single list  
+
+
+d = {}
+print(type(d))
+
+a = {
+    1: "Hello",
+    "name": "Nitesh Kumar",
+    "age" : 23
+}
+
+print(a)
+print(a["name"])
