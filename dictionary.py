44253e3059eebd5fecbc795c8ed5f01c039fd863
Nitesh Kumar <nitesh0221kumar@gmail.com>
2026-08-10 22:14:30 +0530

    dictionary question

--- dictionary.py
+++ dictionary.py
@@ -60,3 +60,61 @@ d = {1: 100, 2: 200 , 3: 300, 4: 400}
 # print(d1)
 
 # help(dict)
+
+
+# d1 = {
+#     10: 100,
+#     20: 200,
+#     30: 300
+# }
+
+# d2 = {
+#     40: 400, 
+#     50: 500,
+#     60: 600
+# }
+
+# for i in d2:
+#     d1[i] = d2[i]
+
+# print(d1)
+
+
+# sum = 0
+# for i in d1:
+#     sum += d1[i]
+
+# print(f"The sum of the values of dictonary is {sum}")
+
+
+# a = [1,2,3,4,2,1,2,3,4,5,7,7,4,4,2,3,2,3,4,2,1,1,1,1,8,8,8]
+
+# d = {}
+# for i in a:
+#     if i in d.keys():
+#         d[i] += 1
+#     else:
+#         d[i] =1
+# print (d)
+
+
+
+d1 = {
+    10: 100,
+    20: 200,
+    40: 300
+}
+
+d2 = {
+    40: 400, 
+    50: 500,
+    60: 600
+}
+
+for i in d2: 
+    if i in d1.keys(): 
+        d1[i] += d2[i]
+    else :
+        d1[i] = d2[i]
+
+print(d1)
