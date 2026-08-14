b58abc49cb9e5bdf71c5fe72e619cacd84a52a3e
Nitesh Kumar <nitesh0221kumar@gmail.com>
2026-08-15 01:19:31 +0530

    classes in oops

---
+++ oops.py
@@ -0,0 +1,27 @@
+# a = 12
+# b = 15
+# print(a + b)        # Imperative Approach
+
+# def add (a , b):
+#     print(a + b)        # Functional Approach
+
+# add(12,12)
+# add(20,30)
+
+############################# Object Oriented Programming Approach (OOP) #############################
+
+# Classes 
+
+class  Animal :
+    species = "DOG"                 # Attribute 
+    a = 12
+
+    def hello (self):
+        print("Hello World")        # Method 
+
+    print("How are you i am getting initialized")
+
+Animal()
+print(Animal().a)
+Animal().hello()
+
