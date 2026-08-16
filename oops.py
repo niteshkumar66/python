bdaed0e0139a1c6fd89ccd7612d69585834762b0
Nitesh Kumar <nitesh0221kumar@gmail.com>
2026-08-16 23:05:22 +0530

    constructors in oops

--- oops.py
+++ oops.py
@@ -19,19 +19,38 @@ class  Animal :
     def hello (self):
         print("Hello World")        # Method 
 
-    print("How are you i am getting initialized")
+    # print("How are you i am getting initialized")
 
-obj = Animal()
+# obj = Animal()
 # print(obj.a)
 # print(obj.species)
 # obj.hello()
 
-obj2 = Animal()
+# obj2 = Animal()
 
-obj3 = Animal()
-obj3.hello()
+# obj3 = Animal()
+# obj3.hello()
 
 # Animal()
 # print(Animal().a)
 # Animal().hello()
 
+
+# constructors 
+
+class  Animal :
+    def __init__(self,type,breed):
+        # print(self)
+        self.type = type
+        self.breed = breed
+
+    def show(self):
+        print(f"Your objects are {self.breed},{self.type}")
+
+dog = Animal("Dog","Pamerian")
+
+cat = Animal("Cat","Black")
+
+print(dog.breed)
+print(cat.type)
+cat.show()
