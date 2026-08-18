1c5a276c404e8149343f3b3ed002343c822eee12
Nitesh Kumar <nitesh0221kumar@gmail.com>
2026-08-18 23:34:16 +0530

    methods and class in oops

--- oops.py
+++ oops.py
@@ -38,20 +38,43 @@ class  Animal :
 
 # constructors 
 
-class  Animal :
-    def __init__(self,type,breed):
-        # print(self)
-        self.type = type
-        self.breed = breed
+# class  Animal :
+#     def __init__(self,type,breed):
+#         # print(self)
+#         self.type = type
+#         self.breed = breed
 
-    def show(self):
-        print(f"Your objects are {self.breed},{self.type}")
+#     def show(self):
+#         print(f"Your objects are {self.breed},{self.type}")
 
-dog = Animal("Dog","Pamerian")
+# dog = Animal("Dog","Pamerian")
 
-cat = Animal("Cat","Black")
+# cat = Animal("Cat","Black")
 
-print(dog.breed)
-print(cat.type)
-cat.show()
-dog.show()
+# print(dog.breed)
+# print(cat.type)
+# cat.show()
+# dog.show()
+
+class Animal : 
+    name = "Lion"               # class attribute  
+
+    def __init__(self,age):
+        self.age = age          # instance attribute 
+
+    def show (self):
+        print(f"How are you ?? , your age is {self.age} ")        # instance method
+
+    @classmethod
+    def hello(cls):
+        print("How are you brother??")
+
+    @staticmethod
+    def hii():
+        print("How are you ? hope good ! ")
+
+obj = Animal(12)
+
+obj.show()
+obj.hello()
+obj.hii()
