a64e9fb65a41feae7604d091f9dc15f89ea27b74
Nitesh Kumar <nitesh0221kumar@gmail.com>
2026-08-19 22:37:22 +0530

    inheritance in oops

--- oops.py
+++ oops.py
@@ -56,25 +56,84 @@ class  Animal :
 # cat.show()
 # dog.show()
 
-class Animal : 
-    name = "Lion"               # class attribute  
+# class Animal : 
+#     name = "Lion"               # class attribute  
 
-    def __init__(self,age):
-        self.age = age          # instance attribute 
+#     def __init__(self,age):
+#         self.age = age          # instance attribute 
 
-    def show (self):
-        print(f"How are you ?? , your age is {self.age} ")        # instance method
+#     def show (self):
+#         print(f"How are you ?? , your age is {self.age} ")        # instance method
 
-    @classmethod
-    def hello(cls):
-        print("How are you brother??")
+#     @classmethod
+#     def hello(cls):
+#         print("How are you brother??")
 
-    @staticmethod
-    def hii():
-        print("How are you ? hope good ! ")
+#     @staticmethod
+#     def hii():
+#         print("How are you ? hope good ! ")
 
-obj = Animal(12)
+# obj = Animal(12)
 
-obj.show()
-obj.hello()
-obj.hii()
+# obj.show()
+# obj.hello()
+# obj.hii()
+
+# class FactoryMumbai:                                # parent class / super class
+#     a = "I am a attribute mentioned inside the factory Mumbai"
+#     def hello (self):
+#         print("Hello i am a method mentioned inside factory Mumbai")
+
+# class FactoryPune (FactoryMumbai):                  # child class / sub class
+#     pass
+
+# obj = FactoryMumbai()
+# obj2 = FactoryPune()
+# print(obj.a)
+# obj2.hello()
+
+
+# class Animal:
+#     def __init__(self,name):
+#         self.name = name
+
+#     def show (self):
+#         print(f"Hello your name is {self.name}")
+
+# class Human(Animal):
+#     def __init__(self, name,age):
+#         super().__init__(name)
+#         self.age = age 
+
+#     def show (self):
+#             print(f"Hello your name is {self.name},{self.age}")
+
+
+# animal1 = Animal("Lion")
+# # animal1.show()
+
+# person1 = Human("Nitesh",23)
+# person1.show()
+
+
+class Animal:
+    name1 = "Lion"
+
+    def __init__(self,name):
+        pass
+
+class Human:
+    name2 = "Nitesh"
+
+    def __init__(self,name,age):
+        pass
+
+class Robots(Animal,Human):
+    name3 = "Charlie123"
+
+
+obj = Robots("Nitesh")
+
+print(obj.name1 )
+print(obj.name2)
+print(obj.name3)
